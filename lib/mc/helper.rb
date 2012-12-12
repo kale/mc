68e9205a611bf8d254574292f1f9d52c97fd27b0
Kale Davis <kale.davis@gmail.com>
2012-12-12 11:40:03 -0500

    created helper module with cli_print

---
+++ lib/mc/helper.rb
@@ -0,0 +1,45 @@
+module Helper
+  def cli_print(input, fields=nil, options={})
+    # set default options
+    options[:show_header] ||= true
+    options[:show_index]  ||= true
+    options[:debug]       ||= false
+
+    # find the correct staring level in the returned json
+    if input.class == Hash
+      input = input["data"] unless input["data"].nil?
+    end
+
+    puts "Fields: #{fields.join(', ')}" unless fields.nil? || !options[:show_header]
+
+    input.to_enum.with_index(1) do |item, index|
+      if options[:debug] || fields.nil?
+        puts "Number of items: #{input.count}"
+        puts "Fields:"
+        item.each {|k,v| puts "#{k} = #{v}"}
+        return
+      else
+        values_to_print = []
+        fields.each do |field|
+          if field.class == Hash
+            values_to_print << item[field.keys.first.to_s][field.values.first.to_s]
+          else
+            values_to_print << item[field.to_s]
+          end
+        end
+
+        print "#{pad(index, input.count)} - " if options[:show_index]
+        puts  "#{values_to_print.join(' ')}"
+      end
+    end
+  end
+
+  private
+
+  def pad(num, total)
+    padding = ""
+    amount_to_pad = total.to_s.size - num.to_s.size
+    amount_to_pad.times {padding << " "}
+    padding + num.to_s
+  end
+end
