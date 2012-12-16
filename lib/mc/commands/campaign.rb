f74aa4853a35a59b9f5fb0a71a852fd2f8156810
Kale Davis <kale.davis@gmail.com>
2012-12-15 23:35:41 -0500

    added create_campaign command

--- lib/mc/commands/campaign.rb
+++ lib/mc/commands/campaign.rb
@@ -1,28 +1,61 @@
 desc 'Campaign related tasks'
 command :campaign do |c|
 
-  c.desc 'Email Address to add/remove'
-  c.flag [:email]
-
   c.desc 'Create Campaign'
   c.command :create do |s|
     c.desc 'List ID'
-    c.flag [:listid]
+    c.flag [:id]
 
     c.desc 'Subject'
     c.flag [:subject]
 
-    c.desc 'From Email'
+    c.desc 'From email'
     c.flag [:from_email]
     
-    c.desc 'to_name'
+    c.desc 'From name'
+    c.flag [:from_name]
+
+    c.desc 'To name'
     c.flag [:to_name]
 
-    c.desc 'authenticate'
-    c.flag [:authenticate]
+    c.desc 'Template ID'
+    c.flag [:template_id] 
 
-    c.desc 'inline_css'
-    c.flag [:inline_css]
+    c.desc 'Gallery template ID'
+    c.flag [:gallery_template_id] 
+
+    c.desc 'Base template ID'
+    c.flag [:base_template_id] 
+
+    c.desc 'Folder ID'
+    c.flag [:folder_id] 
+
+    #c.desc 'Tracking'
+    #c.flag [:tracking] 
+
+    c.desc 'Title - internal name to use'
+    c.flag [:title]
+
+    c.desc 'authenticate'
+    c.flag [:authenticate], :default => true
+
+    #c.desc 'Analytics'
+    #c.flag [:analytics]
+
+    c.desc 'Auto footer?'
+    c.switch :auto_footer, :default => false
+
+    c.desc 'Inline css?'
+    c.switch :inline_css, :default => false
+
+    c.desc 'Auto tweet?'
+    c.switch :tweet, :default => false
+
+    #c.desc 'Auto FB post'
+    #c.flag [:auto_fb_post]
+
+    c.desc 'FB comments?'
+    c.switch :fb_comments, :default => false
 
     c.desc 'HTML filename'
     c.flag [:html_filename]
@@ -45,7 +78,7 @@ command :campaign do |c|
     c.desc 'Send Test Campaign'
     c.switch [:sendtest]    
 
-    s.action do
+    s.action do |global,options,args|
       field, value = options[:seg_options].split(",") if options[:seg_options]
 
       @campaign_id = @mailchimp.campaign_create(options[:listid], options[:subject], options[:html_filename], options[:text_filename], [{:field => field, :op => "like", :value => value}])
@@ -55,7 +88,7 @@ command :campaign do |c|
 
   c.desc 'Send Campaign Now'
   c.command :sendnow do |s|
-    s.action do
+    s.action do |global,options,args|
       campaign_id = options[:cid] || @mailchimp.cache.get(:campaign_id)
       throw "Need a valid Campaign ID." if campaign_id.nil?
 
@@ -65,7 +98,7 @@ command :campaign do |c|
 
   c.desc 'Schedule Campaign'
   c.command :schedule do |s|
-    s.action do
+    s.action do |global,options,args|
       campaign_id = options[:cid] || @campaign_id
       puts @mailchimp.campaign_schedule(campaign_id, "2012-10-24 08:15:00")
     end
@@ -73,7 +106,7 @@ command :campaign do |c|
 
   c.desc 'Send Test Campaign'
   c.command :sendtest do |s|
-    s.action do
+    s.action do |global,options,args|
       campaign_id = options[:cid] || @mailchimp.cache.get(:campaign_id)
       throw "Need a valid Campaign ID." if campaign_id.nil?
 
