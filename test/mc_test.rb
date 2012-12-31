767a718b3e8cc4277163d11949e2abb32d7bf97b
Kale Davis <kale.davis@gmail.com>
2012-12-30 20:46:30 -0500

    added more tests

--- test/mc_test.rb
+++ test/mc_test.rb
@@ -5,10 +5,10 @@ require 'mc'
 require_relative 'test_helper'
 
 describe "MailChimp" do
-	before do
-		@mailchimp = MailChimp.new(config[:apikey])
-		@test_list_id = "ef4227fc80	"
-	end
+  before do
+    @mailchimp = MailChimp.new(config[:apikey])
+    @test_list_id = "ef4227fc80"
+  end
 
   it "should be able to ping MailChimp" do
     @mailchimp.ping.must_equal "Everything's Chimpy!"
@@ -17,12 +17,36 @@ describe "MailChimp" do
   it "should pass to method_missing" do
   	@mailchimp.lists.count.must_be :>, 0
   end
+
+  it "should see updated results" do
+    email = get_random_email_address
+    #see how many subscribers the test list has
+    count = @mailchimp.list_members(:id => @test_list_id)["total"].to_i
+    count.must_be :>=, 0
+
+    #add a new subscriber and make sure the count increases by one
+    @mailchimp.list_subscribe(:id => @test_list_id, :email_address => email, :double_optin => false)
+    new_count = @mailchimp.list_members(:id => @test_list_id)["total"].to_i
+    new_count.must_equal count + 1
+
+    # remove subscriber to reset list
+    @mailchimp.list_unsubscribe(:id => @test_list_id, :email_address => email, :delete_member => true)
+  end
 end
 
 describe "MailChimpCached" do
-	before do
-		@mailchimp = MailChimpCached.new(config[:apikey])
-	end
+  before do
+    @mailchimp = MailChimpCached.new(config[:apikey])
+    @test_list_id = "ef4227fc80"
+    clear_cached_dir
+  end
+
+  it "should not have a cached dir already" do
+  	@mailchimp.lists.count
+ 	File.exists?(cached_dir).must_equal true
+    clear_cached_dir
+ 	File.exists?(cached_dir).must_equal false
+  end
 
   it "should be able to ping MailChimp" do
     @mailchimp.ping.must_equal "Everything's Chimpy!"
@@ -33,12 +57,28 @@ describe "MailChimpCached" do
   end
 
   it "should create a cache dir" do
-  	# delete cache dir
-  	FileUtils.rm_rf(cached_dir)
   	File.exists?(cached_dir).must_equal false
 
   	@mailchimp.lists.count
 
   	File.exists?(cached_dir).must_equal true
+  end
+
+  it "should pull cached results when" do
+    email = get_random_email_address
+
+    count = @mailchimp.list_members(:id => @test_list_id)["total"].to_i
+    count.must_be :>=, 0
+    @mailchimp.list_subscribe(:id => @test_list_id, :email_address => email, :double_optin => false)
+    new_count = @mailchimp.list_members(:id => @test_list_id)["total"].to_i
+    new_count.must_equal count
+    
+    clear_cached_dir
+
+    new_count = @mailchimp.list_members(:id => @test_list_id)["total"].to_i
+    new_count.must_equal count + 1
+
+    # remove subscriber to reset list
+    @mailchimp.list_unsubscribe(:id => @test_list_id, :email_address => email, :delete_member => true)
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,18 @@
 def config
-	config_file = ENV['HOME']+'/.mailchimp'
-	raise "Need API key for testing" unless File.exists?(config_file)
-	YAML.load(File.open(config_file))
+  config_file = ENV['HOME']+'/.mailchimp'
+  raise "Need API key for testing" unless File.exists?(config_file)
+  YAML.load(File.open(config_file))
 end
 
 def cached_dir
-	ENV['HOME']+'/.mailchimp-cache'
+  ENV['HOME']+'/.mailchimp-cache'
+end
+
+def clear_cached_dir
+  # delete cache dir
+  FileUtils.rm_rf(cached_dir)
+end
+
+def get_random_email_address
+  "test+#{rand(1000..9999)}@kaledavis.com"
 end
