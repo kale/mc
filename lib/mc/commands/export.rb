cbf7d655c0c99a2d6252b7f198af865a6f2ad976
Kale Davis <kale.davis@mailchimp.com>
2013-09-03 22:34:45 -0400

    basic export commands

--- lib/mc/commands/export.rb
+++ lib/mc/commands/export.rb
@@ -1,22 +1,36 @@
 desc 'Export'
 command :export do |c|
-  c.desc 'List ID'
-  c.flag :id
-
-  # list ( string apikey, string id, string status array segment string since )
+  # list ( string apikey, string id, string status array segment, string since )
   c.desc 'Exports/dumps members of a list and all of their associated details.'
   c.command :list do |s|
+    c.desc 'List ID'
+    c.flag :id
+
     s.action do |global,options,args|
       id = get_required_argument(:id, options[:id], global[:default_list])
-      puts @mailchimp.export_list(id)
+      @output.standard @mailchimp_cached.export_list(id)
+    end
+  end
+  
+  # ecommOrders ( string since )
+  c.desc 'Dumps all Ecommerce Orders for an account'
+  c.command :ecommm do |s|
+    c.flag :since
+
+    s.action do |global,options,args|
+      @output.standard @mailchimp_cached.export_ecomm_orders
     end
   end
 
-  # helper/search-members (string apikey, string query, string id, int offset)
-  c.desc 'Search account wide or on a specific list using the specified query terms'
-  c.command :members do |s|
+  # campaignSubscriberActivity ( string apikey, string id, boolean include_empty, string since )
+  c.command :campaign do |s|
     s.action do |global,options,args|
-      cli_search_results @mailchimp_cached.helper_search_members(:query => options[:q])
+      c.desc 'Campaign ID'
+      c.flag :cid
+      c.switch :include_empty
+
+      id = get_required_argument(:id, options[:id], global[:default_list])
+      @output.standard @mailchimp_cached.export_campaign_subscriber_activity
     end
   end
 end
